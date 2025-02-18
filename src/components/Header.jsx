19d3435fdaf57f2215d2004cc2ef84cc95b42161
Luthfi <kankagate@gmail.com>
2025-02-19 03:15:50 +0700

    112. Scoping CSS Rules with CSS module

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import logo from '../assets/logo.png';
-import './Header.css';
+import style from './Header.module.css';
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p className={style.paragraph}>A community of artists and art-lovers.</p>
     </header>
   );
 }
